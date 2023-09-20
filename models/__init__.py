27994e870bc4cb1f3ec8ad1c1e32e15823edb1cd
joelsteven <joelofelectronics@gmail.com>
2023-09-21 01:18:35 +0300

    Fixed init file

--- models/__init__.py
+++ models/__init__.py
@@ -5,11 +5,11 @@ from os import getenv
 
 HBNB_TYPE_STORAGE = getenv('HBNB_TYPE_STORAGE')
 
-if HBNB_TYPE_STORAGE != 'db':
-    from models.engine.file_storage import FileStorage
-    storage = FileStorage()
-else:
+if HBNB_TYPE_STORAGE == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
-
-storage.reload()
+    storage.reload()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+    storage.reload()
