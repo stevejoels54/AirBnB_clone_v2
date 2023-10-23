9118b0fb0db20010d7cb3e5391b892e719d080d0
joelsteven <joelofelectronics@gmail.com>
2023-10-23 05:04:30 +0300

    Added task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Starts Flask web app """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbn():
+    """ Returns Hello HBNB! """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Returns HBNB """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """ Returns C and value of text """
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text="is cool"):
+    """ Returns Python and vale of the text """
+    text = text.replace('_', ' ')
+    return "Python {}".format(text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
