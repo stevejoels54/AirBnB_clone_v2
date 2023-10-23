25ddde677832e46cdba3dce62320d3fc94ab97f0
joelsteven <joelofelectronics@gmail.com>
2023-10-23 05:56:56 +0300

    Added storage engine dependant tasks

---
+++ web_flask/9-states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Starts a Flask web application on 0.0.0.0, port 5000 """
+from models import storage
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/states", strict_slashes=False)
+def states():
+    """ Displays HTML page with a list of all States.
+    States are sorted by name.
+    """
+    states = storage.all("State")
+    return render_template("9-states.html", state=states)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def states_id(id):
+    """ Displays HTML page with info about <id>, if it exists """
+    for state in storage.all("State").values():
+        if state.id == id:
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html")
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """ Remove SQLAlchemy session """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
