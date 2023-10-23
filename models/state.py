131801ae564ea00ef7c58376df70d20c51954f0f
joelsteven <joelofelectronics@gmail.com>
2023-10-23 05:40:54 +0300

    Added storage engine fixes

--- models/state.py
+++ models/state.py
@@ -1,13 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+import models
+from os import getenv
+from models.base_model import Base
+from models.base_model import BaseModel
+from sqlalchemy import Column
+from models.city import City
+from sqlalchemy import String
 from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = 'states'
-
+    """Represents a State"""
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state', cascade='all, delete')
+    cities = relationship("City", backref="state", cascade="delete")
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """Get a list of all related Cities."""
+            city_l = []
+            for city in list(models.storage.all(City).values()):
+                if city.state_id == self.id:
+                    city_l.append(city)
+            return city_l
