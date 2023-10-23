91ee995a00a3cb60d3a0483ac1e8366c68482acb
joelsteven <joelofelectronics@gmail.com>
2023-10-23 05:01:39 +0300

    Added task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Starts Flask web app """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbn():
+    """ Returns Hello HBNB! """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Returns HBNB """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """ Returns C and value of text """
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
